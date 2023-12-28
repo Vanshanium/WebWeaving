5f197353f45a6944d730b8d0263eef347f2e1f4e
Venzene <vansh.singh8272@gmail.com>
2023-12-28 23:27:21 +0530

    Man I think I am pretty slowgit add --allgit add --allgit add --all I have to rushhhhh

--- The_watcher/src/main.jsx
+++ The_watcher/src/main.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+// It just searches for the root in the html DOM and renders whatever is in <App>
+// document is a special object which represent the entire html
+
