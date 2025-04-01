db1890c76c3c552e2deb6ce65db79b4f150bdb80
Voob <vansh.singh8272@gmail.com>
2025-04-01 12:39:24 +0530

    Cleaning

---
+++ Learning/myscript.js
@@ -0,0 +1,16 @@
+let elemet = document.getElementById("Cliche_Code"); 
+//  Get a element 
+
+/*
+Change the content using this 
+
+// Call functions when the Event Occurs 
+
+element.addEventListener('eventType', callbackFunction, useCapture);
+
+
+*/
+
+
+
+alert(location.href = "https://google.com");
